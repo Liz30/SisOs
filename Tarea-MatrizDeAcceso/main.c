ca2a390f4aaa5141a13669f2f14a91b6373c459e
LizZ <villeda.e@gmail.com>
2016-03-10 18:29:40 -0600

    MatrizDeAcceso
    Inicio

---
+++ Tarea-MatrizDeAcceso/main.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "matriz.h"
+
+#define NameSize 10
+
+void CrearMatriz();
+void MountDiscMenu();
+void FormatDiscMenu();
+void DeleteDiscMenu();
+void AllocateBlockMenu();
+void FreeBlockMenu();
+void GetTableMenu();
+
+int main(){
+
+  int op = 0;
+  while (op!=8){
+    system("cls");
+    printf("\n  1. Crear Matriz \n  2. Montar Disco \n  3. Formatear Disco");
+    printf("\n  4. Eliminar Disco\n  5. Alocar Bloque\n  6. Liberar Bloque");
+    printf("\n  7. Ver Tabla\n  8. Salir     > ");
+    scanf("%d", &op);
+    switch (op) {
+        case 1: CrearMatriz(); op = 0; break ;
+        case 2: MountDiscMenu(); op = 0 ; break;
+        case 3: FormatDiscMenu(); op = 0 ; break;
+        case 4: DeleteDiscMenu(); op =0 ; break;
+        case 5: AllocateBlockMenu(); op = 0; break;
+        case 6: FreeBlockMenu(); op = 0; break;
+        case 7: GetTableMenu(); op = 0; break;
+        case 8: ;
+    }
+  }
+  return 0;
+}
+
+void CrearMatriz(){
+    char fname[NameSize] = "disc1";
+    int dsize, bsize;
+    //printf("\n   Nombre del Disco (Max. 10 caracteres) > " );
+    //scanf("%10s", &fname );
+    printf("\n   Tamaño del Disco (MB) > " );
+    scanf("%d", &dsize );
+    printf("   Tamaño del Bloque (KB) > " );
+    scanf("%d", &bsize );
+
+    int ferror = CreateDisc(fname, dsize, bsize);
+    if (ferror != 0)
+        printMsg(" El disco ya existe o no se completo su formato.");
+    else
+        printMsg(" El Disco fue creado. ");
+}
+
+void MountDiscMenu(){
+
+  /*   fp=fopen("archivo.txt","r");
+     if(fp==NULL)
+     {
+          printf("Error al abrir el archivo para leer");
+          exit(1);
+     }
+     fclose(fp);*/
+}
+
+void FormatDiscMenu(){
+  char fname[NameSize];
+  printf("\n FORMAT - Nombre del Disco (Max. 10 caracteres) > " );
+  scanf("%10s", &fname );
+  int form = FormatDisc(fname);
+  if (form != 0 ) //Error
+    printMsg("Formato Incompleto...");
+}
+
+void DeleteDiscMenu(){
+  char fname[NameSize];
+  printf("\n ELIMINAR -  Nombre del Disco (Max. 10 caracteres) > " );
+  scanf("%10s", &fname );
+  int form = DeleteDisc(fname);
+  if (form != 0 ) //Error
+    printMsg("Disco NO fue eliminado...");
+  else
+    printMsg("Disco fue eliminado exitosamente...");
+}
+
+void AllocateBlockMenu(){
+  int n;
+  printf("\n   Cantidad de Bloques > " );
+  scanf("%d", &n );
+  AllocateBlocks(n);
+}
+
+void FreeBlockMenu(){
+  int n;
+  printf("\n   Posicion de Bloque > " );
+  scanf("%d", &n );
+  FreeBlock(n);
+}
+
+void GetTableMenu(){
+  getTable();
+  getch();
+}
